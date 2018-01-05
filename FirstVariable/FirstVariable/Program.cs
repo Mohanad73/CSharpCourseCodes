df3285d0b9a7a1b6358af74ae5e0628cc91cc7ae
Mohanad73 <35133869+Mohanad73@users.noreply.github.com>
2018-01-05 16:55:29 +0300

    First Vailable Codes
    this is my first Vailable codes

---
+++ FirstVariable/FirstVariable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is my first VariableCodes
+            int x = 25;
+            Console.WriteLine("Variable Code = {0}", x);
+            Console.ReadKey();
+
+        }
+    }
+}
